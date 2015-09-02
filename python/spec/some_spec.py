acafe715c3d214cf1819b097900b711c1640fbed
alejandrodob <alejandrodob@gmail.com>
2015-09-02 19:37:24 +0200

    Python mamba boilerplate

---
+++ python/spec/some_spec.py
@@ -0,0 +1,11 @@
+with description('something'):
+
+    with context('when someting happens'):
+
+        with before.each:
+            pass
+        with before.all:
+            pass
+
+        with it('does something'):
+            pass
